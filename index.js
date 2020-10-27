f2a915e2e3bbf635413f1a7e5fcd134b7a34ff66
KouKiriyama03 <josuepanky@gmail.com>
2020-10-27 00:34:46 -0600

    se creo el index para el end point en /

--- index.js
+++ index.js
@@ -1,8 +1,28 @@
 // Importar express
 const express = require("express");
 
+// Importar handlebars como template engine
+const exphbs = require("express-handlebars");
+
+// se declara y ya con la de abajo se manda a pantalla
+const path = require("path");
+
+
 // Crear un servidor express
 const app = express();
+
+// Indicar a express utilizar handlebars como template engine
+app.engine("hbs", exphbs({ defaultLayout: "main", extname: ".hbs" }));
+
+app.set("view engine", "hbs");
+
+// Llama las cosas que estan estaticas
+app.use(express.static(path.join(__dirname, "public")));
+// jala archivisto de la vista
+app.get("/", (req, res, next) => {
+res.render("index");
+});
+
 
 // Inicializar el servidor en un puerto en específico
 app.listen(5005, () => {
